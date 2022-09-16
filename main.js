2bf6481ab33690eb67348ac336eb0a7abbfe77dd
robinbouma7 <robinbouma711@gmail.com>
2022-09-16 19:04:15 +0200

    added reading data from xml file

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 //include packages
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+//xml parser
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
+//filesystem
+const fs = require('fs');
 
 //class met info over een profile
 //info krijg je van config.json van ferium
-class profile {
+//name: naam van het profiel
+//version: minecraft versie
+//modloader: type modloader (fabric, forge, etc.)
+//foldername: naam van de folder in de multimc instances folder
+//mods: een array van Mod classes
+class Profile {
 	name;
 	version;
 	modloader;
 	flodername;
-	mods = [];
+	mods;
 }
+//class met info over mods
+//name: naam van de mod
+//identifier: project id(modrinth, curseforge), mod naam(modrinth) of repository naam
+//type: cusreforge, modrinth of github
+class Mod {
+	name;
+	identifier;
+	type;
+}
+var mmcpath;
+var profiles;
 
 app.whenReady().then(() => {
 	createWindow();
+
+	loadxml("./config.xml");
 
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -44,6 +67,29 @@ const createWindow = () => {
 	//load index.html in that window
 	win.loadFile('index.html');
 }
+//load confix xml file
+//path: path naar xml bestand
+function loadxml(path) {
+	var data;
+	console.log(`xml file path: ${path}`);
+	//zet de data in bestand over naar file
+	var file = fs.readFileSync(path, "utf8");
+	//parse xml data uit file
+	parser.parseString(file, function(error, result) {
+        if(error === null) {
+            console.log(result);
+        }
+        else {
+            console.log(error);
+			return;
+        }
+        data = result
+    });
+	//verwerk de data uit het xml bestand
+	setmmcpath(data.info.mmcpath);
+	console.log(`number of profiles ${data.info.profilenum}`);
+}
+
 //create a profile with these parameters
 //name: name of the profile
 //version: minecraft version
@@ -52,16 +98,19 @@ const createWindow = () => {
 function createprofile(name, version, modloader, flodername) {
 
 }
+
 //switch to another profile
 //name: name of the profile to switch to
 function switchprofile(name) {
 
 }
+
 //delete a profile
 //name: name of the profile to delete
 function deleteprofile(name) {
 
 }
+
 //edit a profile, overwrites all the info with the info given
 //name: name of the profile
 //version: minecraft version
@@ -70,27 +119,33 @@ function deleteprofile(name) {
 function editprofile(name, version, modloader, flodername) {
 
 }
+
 //add a mod to a profile
 //type: modrinth, curseforge or github
 //identifier: project id(modrinth, curseforge), mod name(modrinth) or repository name
 function addmod(type, identifier) {
 
 }
+
 //removes a mod
 //name: name of the mod
 function removemod(name) {
 
 }
+
 //update all the mods in the current profile
 function updatemods() {
 
 }
+
 //launch multimc
 function launchmmc() {
 
 }
+
 //sets the path of multimc
 //path: path to the folder where multimc.exe is located
 function setmmcpath(path) {
-
+	console.log(`multimc path set to ${path}`);
+	mmcpath = path;
 }
