9a62c313fd8a827428db7cb6eae138df23a20088
robinbouma7 <robinbouma711@gmail.com>
2022-09-17 12:50:44 +0200

    documented one more thing

--- renderer.js
+++ renderer.js
@@ -1,8 +1,9 @@
 //browser side code
 
-//launch m,c element
+//launch mmc element
 const launchmmc = document.getElementById('launchmmc');
 
+//je moet het zo doen, je kan niet een functie van hier runnen vanaf html
 launchmmc.addEventListener("click", async () => {
     window.electronAPI.launchmmc();
 });
