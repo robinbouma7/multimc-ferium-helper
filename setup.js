2a2a4fb68b501d38f502f4bc07d08564632c94b9
robinbouma7 <robinbouma711@gmail.com>
2022-09-17 14:03:58 +0200

    file selecting required to submit setup

--- setup.js
+++ setup.js
@@ -1,5 +1,14 @@
 const submitbutton = document.getElementById('submitbutton');
 
+document.getElementById("warntext").style.visibility = "hidden";
+document.getElementById("warntext").style.color = "red";
+
 submitbutton.addEventListener("click", async () => {
-    window.electronAPI.submitsetup(document.getElementById("multimcpath").files[0].path);
+    if(document.getElementById("multimcpath").value == undefined || document.getElementById("multimcpath").value == "") {
+        document.getElementById("warntext").style.visibility = "visible";
+    }
+    else {
+        window.electronAPI.submitsetup(document.getElementById("multimcpath").files[0].path);
+    }
+    
 });
